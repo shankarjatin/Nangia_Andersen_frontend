099879fa04a8bf8e692bd12baca13ed177557d42
shankarjatin <shankarjatin1005@gmail.com>
2024-06-17 17:13:12 +0530

    register auth component

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { register } from '../../actions/authActions';
+
+const Register = ({ history }) => {
+  const [formData, setFormData] = useState({
+    companyName: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+  const dispatch = useDispatch();
+
+  const { companyName, email, password, password2 } = formData;
+
+  const onChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.error('Passwords do not match');
+    } else {
+      dispatch(register({ companyName, email, password }));
+      history.push('/candidates');
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div>
+        <input
+          type="text"
+          name="companyName"
+          value={companyName}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password2"
+          value={password2}
+          onChange={onChange}
+          required
+        />
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
